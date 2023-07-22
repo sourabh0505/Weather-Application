67f6ca2b650d95fb222b193bf95311a4a822c431
sourabh0505 <sourabhvishnoi05@gmail.com>
2023-07-22 15:15:23 +0530

    weather-application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import './App.css'
+
 
 function App() {
+  const [weatherData, setWeatherData] = useState('');
+  
+  const [location, setlocation] = useState('');
+  
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=e94203882f1068d1f5dfefcbb4d365cd`;
+
+  const fetchLocation = (event) =>{
+    fetch(apiUrl)
+    .then((response) =>{
+      return response.json()
+    }).then((data) =>{
+      setWeatherData(data)
+      console.log(data)
+    })
+    setlocation('')
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className="search">
+        <input className='searchbar' type="text" placeholder='Type your location here' value={location} onChange={event => setlocation(event.target.value)}/>
+        <button className="searchbtn" onClick={fetchLocation}>Search</button>
+      </div>
+
+      <div className="locationandtemperature">
+      <div className="cityandcountry">
+        <h1>{weatherData.name}</h1>
+        {weatherData.sys ? <h2 className='values'>{weatherData.sys.country}</h2> : null}
+      </div>
+      
+      <div className="temperature">
+        {weatherData.main ? <h1>{weatherData.main.temp} °F</h1> : null}
+        {weatherData.weather ? <h3 className='values'>{weatherData.weather[0].description}</h3> : null}
+      </div>
+      </div>
+{weatherData.name != undefined && 
+      <div className='multiplecontent'>
+        <div className="feelslike">
+          {weatherData.main ? <h2 className='values'>{weatherData.main.feels_like} °F</h2> : null}
+          <h1>Feels Like</h1>
+        </div>
+
+        <div className="humidity">
+          {weatherData.main ? <h2 className='values'>{weatherData.main.humidity} %</h2> : null}
+          <h1>Humidity</h1>
+        </div>
+
+        <div className="windspeed">
+          {weatherData.wind ? <h2 className='values'>{weatherData.wind.speed} MPH</h2> : null}
+          <h1>Wind Speed</h1>
+        </div>
+      </div>
+}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
